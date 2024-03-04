2e5979c599bdc0531825b71b07daaa426529399a
Nico-Serra <nico.serra000@gmail.com>
2024-03-04 12:51:46 +0100

    init commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+console.log('Hello world');
+
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+//- Cicliamo all'interno dell'array Team e ci logghiamo le info di ogni singolo member 
+//- Stampiamo all'interno della DOM queste info per ogni member
+    ///BONUS///
+//- Ci creiamo una let markup dove all'interno prendiamo parte del codice HTML e con il tamplate litteral inseriamo la variabile che contiene la foto 
+//- Modifichiamo il markup per poter organizzare le card dei member
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Instruction
+
+Viene fornito un file js in cui è definito un array di oggetti che rappresentano i membri del team.
+
+Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+
+MILESTONE 1:
+
+stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+MILESTONE 2:
+
+stampare le stesse informazioni su DOM sottoforma di stringhe
+
+BONUS 1:
+
+trasformare la stringa foto in una immagine effettiva
+
+BONUS 2:
+
+organizzare i singoli membri in card/schede
+
+## Steps
+
+- Cicliamo all'interno dell'array Team e ci logghiamo le info di ogni singolo member 
+- Stampiamo all'interno della DOM queste info per ogni member
+///BONUS///
+- Ci creiamo una let markup dove all'interno prendiamo parte del codice HTML e con il tamplate litteral inseriamo la variabile che contiene la foto 
+- Modifichiamo il markup per poter organizzare le card dei member
+
+## Tools
+
+- for
+- forin
+- innerHTML
+- log
